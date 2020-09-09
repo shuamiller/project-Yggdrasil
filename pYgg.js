f7f66a69bc843b43959fd46688a63d095c54d984
Shua Miller <thejoshmill@gmail.com>
2020-09-09 13:16:19 -0700

    Fix typo in 'entryPoint' description.

--- pYgg.js
+++ pYgg.js
@@ -8,7 +8,7 @@ let Worlds = [
 
 let entryPoint = {
     'name': "Entry Point",
-    'description': "You find yourself in a romm of smooth stone the color of sand.\
+    'description': "You find yourself in a room of smooth stone the color of sand.\
     You don't remember how you came to be here. You don't remember who you are.\n\
     This room seems to be illuminated without a source. Ahead of you, there is a\
     recessed wall that could be a doorway, but there is no opening. To the left\
