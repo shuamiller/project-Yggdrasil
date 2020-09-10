641734f33ea9cbe1244a17a9c1caf9ffc67b036b
Shua Miller <thejoshmill@gmail.com>
2020-09-09 17:26:03 -0700

    Change 'gameText.textContent' to 'entryPoint.description'. Reordered program.

--- pYgg.js
+++ pYgg.js
@@ -3,6 +3,41 @@ const beginDiv = document.querySelector("#begin-div");
 const textDiv = document.querySelector("#text-div");
 const playerInterface = document.querySelector("#player-interface");
 const inputDiv = document.querySelector("#input-div");
+let currentRoom;
+
+
+let doorway = {
+
+};
+
+let darkGlass = {
+
+};
+
+let heraldOfYggdrasil = {
+
+};
+
+let entryPoint = {
+    name: "Entry Point",
+    description: "You find yourself in a room of smooth stone the color of sand. You don't remember how you came to be here. You don't remember who you are.\n This room seems to be illuminated without a source. Ahead of you, there is a recessed wall that could be a doorway, but there is no opening. To the left of the recess, a circular portal of dark glass is set in the wall.",
+    objects: [
+        doorway,
+        darkGlass
+    ],
+    characters: [
+        heraldOfYggdrasil
+    ]
+};
+
+let Worlds = [
+    { //Worlds[0]
+        'Rooms': [
+            entryPoint
+        ] 
+    }
+]
+
 
 beginBtn.addEventListener('click', () => {
     playerInterface.removeChild(beginDiv);
@@ -19,26 +54,6 @@ beginBtn.addEventListener('click', () => {
     }
     const gameText = document.createElement('p');
     gameText.setAttribute('class', 'game-text');
-    gameText.textContent = `You find yourself in a room of smooth stone the color of sand. You don't remember how you came to be here. You don't remember who you are.\n This room seems to be illuminated without a source. Ahead of you, there is a recessed wall that could be a doorway, but there is no opening. To the left of the recess, a circular portal of dark glass is set in the wall.`;
+    gameText.textContent = entryPoint.description;
     textDiv.appendChild(gameText);
-})
-
-let Worlds = [
-    { //Worlds[0]
-        'Rooms': [
-            entryPoint
-        ] 
-    }
-]
-
-let entryPoint = {
-    name: "Entry Point",
-    description: "You find yourself in a room of smooth stone the color of sand. You don't remember how you came to be here. You don't remember who you are.\n This room seems to be illuminated without a source. Ahead of you, there is a recessed wall that could be a doorway, but there is no opening. To the left of the recess, a circular portal of dark glass is set in the wall.",
-    objects: [
-        doorway,
-        darkGlass
-    ],
-    characters: [
-        heraldOfYggdrasil
-    ]
-};
+});
