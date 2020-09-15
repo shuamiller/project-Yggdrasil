e49a9b8bd744acaacfb95f5ceb665720ae4f88ec
Shua Miller <thejoshmill@gmail.com>
2020-09-14 17:40:07 -0700

    Add extra '\n' to 'entryPoint' description.

--- pYgg.js
+++ pYgg.js
@@ -34,7 +34,7 @@ let heraldOfYggdrasil = {
 
 let entryPoint = {
     name: "Entry Point",
-    description: "You find yourself in a room of smooth stone the color of sand. You don't remember how you came to be here. You don't remember who you are.\n This room seems to be illuminated without a source. Ahead of you, there is a recessed wall that could be a doorway, but there is no opening. To the left of the recess, a circular portal of dark glass is set in the wall.",
+    description: "You find yourself in a room of smooth stone the color of sand. You don't remember how you came to be here. You don't remember who you are.\n\nThis room seems to be illuminated without a source. Ahead of you, there is a recessed wall that could be a doorway, but there is no opening. To the left of the recess, a circular portal of dark glass is set in the wall.",
     objects: [
         doorway,
         darkGlass
